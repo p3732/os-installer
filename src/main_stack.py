129098e0e3d282f9b5a519727b3e0cc1a578037d
Peter <650-p3732@users.noreply.gitlab.gnome.org>
2021-01-07 03:42:27 +0100

    main stack: small logic simplification

--- src/main_stack.py
+++ src/main_stack.py
@@ -73,8 +73,7 @@ class MainStack(Gtk.Box):
         elif retVal == 'waiting':
             self.next_stack.set_visible_child_name('waiting')
         elif retVal == 'automatic':
-            self._make_accessible(self.current + 1)
-            self._load_page(self.current + 1)
+            self._go_to_next()
         else:
             self._update_buttons()
 
