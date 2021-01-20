6320bb4c29ff190535df3f1933ed6c67f8f5a482
Peter <650-p3732@users.noreply.gitlab.gnome.org>
2021-01-20 02:51:03 +0100

    config: fix attribute checking

--- src/config.py
+++ src/config.py
@@ -12,26 +12,18 @@ def get_config():
     return config
 
 
-def _get_fallback_config():
-    return {
-        'internet_checker_url': 'http://nmcheck.gnome.org/check_network_status.txt',
-        'suggested_languages': ["en", "ar", "de", "es", "fr", "ja", "ru", "zh"],
-        'additional_software': {}
-    }
-
-
 def check_install_config(config):
-    return (hasattr(config, 'formats') and
-            hasattr(config, 'disk_device_path') and
-            hasattr(config, 'disk_is_partition') and
-            hasattr(config, 'use_encryption') and
-            hasattr(config, 'encryption_pin'))
+    return ('locale' in config and
+            'disk_device_path' in config and
+            'disk_is_partition' in config and
+            'use_encryption' in config and
+            'encryption_pin' in config)
 
 
 def check_post_install_config(config):
-    return (hasattr(config, 'user_name') and
-            hasattr(config, 'user_password') and
-            hasattr(config, 'locale') and
-            hasattr(config, 'formats') and
-            hasattr(config, 'timezone') and
-            hasattr(config, 'additional_software'))
+    return ('user_name' in config and
+            'user_password' in config and
+            'locale' in config and
+            'formats' in config and
+            'timezone' in config and
+            'additional_software' in config)
