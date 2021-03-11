e0df1aae445495598351b62f1125eb3baface317
Peter <650-p3732@users.noreply.gitlab.gnome.org>
2021-03-11 02:03:32 +0100

    software: fix base type to overlay

--- src/ui/pages/software.py
+++ src/ui/pages/software.py
@@ -8,7 +8,7 @@ from gi.repository import Gtk
 
 
 @Gtk.Template(resource_path='/com/github/p3732/os-installer/ui/pages/software.ui')
-class SoftwarePage(Gtk.Box, Page):
+class SoftwarePage(Gtk.Overlay, Page):
     __gtype_name__ = __qualname__
     image_name = 'system-software-install-symbolic'
 
@@ -17,7 +17,7 @@ class SoftwarePage(Gtk.Box, Page):
     continue_button = Gtk.Template.Child()
 
     def __init__(self, global_state, **kwargs):
-        Gtk.Box.__init__(self, **kwargs)
+        Gtk.Overlay.__init__(self, **kwargs)
 
         self.global_state = global_state
 
