43ace719d39202c2a15474be1bcd7f4fc1da1c78
Peter <650-p3732@users.noreply.gitlab.gnome.org>
2021-03-18 17:02:27 +0100

    check and print all variables in example scripts

--- example_config/scripts/configure.sh
+++ example_config/scripts/configure.sh
@@ -2,12 +2,8 @@
 
 # This is an example configuration script. For OS-Installer to use it, place it at:
 # /etc/os-installer/scripts/configure.sh
-# The script gets called with the following environment variables set:
-# OSI_LOCALE             : Locale to be used in the new system
-# OSI_DEVICE_PATH        : Device path at which to install
-# OSI_DEVICE_IS_PARTITON : 1 if the specified device is a partition (0 -> disk)
-# OSI_USE_ENCRYPTION     : 1 if the installation is to be encrypted
-# OSI_ENCRYPTION_PIN     : The encryption pin to use (if encryption is set)
+# The script gets called with the environment variables from the install script
+# (see install.sh) and these additional variables:
 # OSI_USER_NAME          : User's name. Not ASCII-fied
 # OSI_USER_AUTOLOGIN     : Whether to autologin the user. If set, no password is given
 # OSI_USER_PASSWORD      : User's password. Not given if autologin is set
@@ -15,14 +11,42 @@
 # OSI_TIMEZONE           : Timezone to be used
 # OSI_ADDITIONAL_SOFTWARE: Space-separated list of additional packages to install
 
-#TODO all variables
-if [ -z ${OSI_DEVICE_PATH+x} ]
+# sanity check that all variables were set
+if [ -z ${OSI_LOCALE+x} ] || \
+   [ -z ${OSI_DEVICE_PATH+x} ] || \
+   [ -z ${OSI_DEVICE_IS_PARTITION+x} ] || \
+   [ -z ${OSI_DEVICE_EFI_PARTITION+x} ] || \
+   [ -z ${OSI_USE_ENCRYPTION+x} ] || \
+   [ -z ${OSI_ENCRYPTION_PIN+x} ] || \
+   [ -z ${OSI_USER_NAME+x} ] || \
+   [ -z ${OSI_USER_AUTOLOGIN+x} ] || \
+   [ -z ${OSI_USER_PASSWORD+x} ] || \
+   [ -z ${OSI_FORMATS+x} ] || \
+   [ -z ${OSI_TIMEZONE+x} ] || \
+   [ -z ${OSI_ADDITIONAL_SOFTWARE+x} ]
 then
-    echo "Configuration script called without all environment variables set!"
+    echo "Installer script called without all environment variables set!"
     exit 1
 fi
 
 echo 'Configuration started.'
+
+echo 'Installation started.'
+echo ''
+echo 'Variables set to:'
+echo 'OSI_LOCALE               ' $OSI_LOCALE
+echo 'OSI_DEVICE_PATH          ' $OSI_DEVICE_PATH
+echo 'OSI_DEVICE_IS_PARTITION  ' $OSI_DEVICE_IS_PARTITION
+echo 'OSI_DEVICE_EFI_PARTITION ' $OSI_DEVICE_EFI_PARTITION
+echo 'OSI_USE_ENCRYPTION       ' $OSI_USE_ENCRYPTION
+echo 'OSI_ENCRYPTION_PIN       ' $OSI_ENCRYPTION_PIN
+echo 'OSI_USER_NAME            ' $OSI_USER_NAME
+echo 'OSI_USER_AUTOLOGIN       ' $OSI_USER_AUTOLOGIN
+echo 'OSI_USER_PASSWORD        ' $OSI_USER_PASSWORD
+echo 'OSI_FORMATS              ' $OSI_FORMATS
+echo 'OSI_TIMEZONE             ' $OSI_TIMEZONE
+echo 'OSI_ADDITIONAL_SOFTWARE  ' $OSI_ADDITIONAL_SOFTWARE
+echo ''
 
 # Pretending to do something
 echo 'Pretending to do something'
