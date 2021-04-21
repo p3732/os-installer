944cdf0309ae162a7b21958037fcf15500c52ce1
Peter <650-p3732@users.noreply.gitlab.gnome.org>
2021-04-21 05:01:31 +0200

    don't falsely proclaim time synchronisation

--- example_config/scripts/prepare.sh
+++ example_config/scripts/prepare.sh
@@ -3,7 +3,6 @@
 # This is an example preparation script. For OS-Installer to use it, place it at:
 # /etc/os-installer/scripts/prepare.sh
 # The script gets called when an active internet connection was established.
-# The system time might not be synchronized just yet, but should be within seconds.
 
 echo 'Preparation started.'
 
