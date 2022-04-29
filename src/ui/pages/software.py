1123264ddc3afc547b15d8dfa0345c0e07e338aa
Peter Eisenmann <p3732@getgoogleoff.me>
2022-04-29 14:05:42 +0200

    software: make rows GListModel based

--- src/ui/pages/software.py
+++ src/ui/pages/software.py
@@ -1,11 +1,11 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from gi.repository import Gtk
+from gi.repository import Gio, Gtk
 
 from .global_state import global_state
 from .page import Page
 from .software_provider import get_software_suggestions
-from .widgets import SoftwareRow
+from .widgets import reset_model, SoftwareRow
 
 
 @Gtk.Template(resource_path='/com/github/p3732/os-installer/ui/pages/software.ui')
@@ -14,16 +14,15 @@ class SoftwarePage(Gtk.Box, Page):
     image_name = 'system-software-install-symbolic'
 
     software_list = Gtk.Template.Child()
-
+    software_model = Gio.ListStore()
 
     def __init__(self, **kwargs):
         Gtk.Box.__init__(self, **kwargs)
+        self.software_list.bind_model(self.software_model, lambda o: SoftwareRow(o.package))
 
     def _setup_software(self):
         suggestions = get_software_suggestions()
-        for package in suggestions:
-            row = SoftwareRow(package)
-            self.software_list.append(row)
+        reset_model(self.software_model, suggestions)
 
     ### callbacks ###
 
--- src/util/provider/software_provider.py
+++ src/util/provider/software_provider.py
@@ -1,6 +1,16 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from gi.repository import GObject
 from .global_state import global_state
+
+
+class Package(GObject.GObject):
+    __gtype_name__ = __qualname__
+
+    def __init__(self, package):
+        super().__init__()
+
+        self.package = package
 
 
 ### public methods ###
@@ -14,7 +24,7 @@ def get_software_suggestions():
                 'name' in package and
                 'description' in package and
                     'icon_path' in package):
-                suggestions.append(package)
+                suggestions.append(Package(package))
             else:
                 print(f'Package {package} not correctly configured!')
     return suggestions
