72707b2e53ce4bc2c1905a3b36f53eb79d4de410
Peter Eisenmann <p3732@getgoogleoff.me>
2022-04-30 12:01:55 +0200

    keyboard: wrap keyboard info in helper struct

--- src/util/provider/keyboard_layout_provider.py
+++ src/util/provider/keyboard_layout_provider.py
@@ -4,16 +4,25 @@ from gi.repository import GObject
 from gi.repository.GnomeDesktop import XkbInfo
 
 
+class KeyboardInfo(GObject.GObject):
+    __gtype_name__ = __qualname__
+    name: str
+    layout: str
+
+    def __init__(self, name, layout):
+        super().__init__()
+
+        self.name = name
+        self.layout = layout
+
 def get_layouts_for(language_short_hand, language):
     xkb_info = XkbInfo()
     layouts = xkb_info.get_layouts_for_language(language_short_hand)
 
     named_layouts = []
     for layout in layouts:
-        o = GObject.GObject()
-        o.name = xkb_info.get_layout_info(layout).display_name
-        o.layout = layout
-        named_layouts.append(o)
+        name = xkb_info.get_layout_info(layout).display_name
+        named_layouts.append(KeyboardInfo(name, layout))
 
     return named_layouts
     # TODO sorting
